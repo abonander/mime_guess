123d9bf44525a8c9c9920065775b2a5b3d6cae50
Austin Bonander <austin.bonander@gmail.com>
2015-02-27 15:04:53 -0800

    Use static list of extension-MIME type mappings

--- src/lib.rs
+++ src/lib.rs
@@ -1,81 +1,86 @@
 //! Guessing of MIME types by file extension.
 //!
-//! Extension-to-MIME-type mappings are loaded from a JSON file at runtime
-//! and stored in a thread-local heap. To limit memory usage, avoid calling
-//! functions in this module from several threads.
-//!
-//! TODO: Make mapping cache global in a safe manner.
+//! Uses a static list of file-extension : MIME type mappings.
+#![feature(os, path)]
+extern crate mime;
 
-use mime::{Mime, TopLevel, SubLevel};
+use mime::Mime;
 
-use serialize::json::{mod, Json};
+pub use mime_types::MIME_TYPES;
 
-use std::collections::HashMap;
+use std::ascii::AsciiExt;
+use std::ffi::OsStr;
+use std::path::Path;
+
+mod mime_types;
 
 /// Guess the MIME type of the `Path` by its extension.
 ///
-/// **Guess** is the operative word here, as the contents of a file
-/// may not necessarily match its MIME type/extension.
+/// If the given `Path` has no extension, or its extension has no known MIME type mapping,
+/// then the MIME type is assumed to be `application/octet-stream`.
+///
+/// ##Note
+/// *Guess* is the operative word here, as there are no guarantees that the contents of the file 
+/// that `path` points to match the known MIME type of the path's extension. 
+/// Take care when processing files with assumptions based on the return value of this method.
 pub fn guess_mime_type(path: &Path) -> Mime {
-    let ext = path.extension_str().unwrap_or("");
+    let ext = path.extension().and_then(OsStr::to_str).unwrap_or("");
 
     get_mime_type(ext)
 }
-
-/// Extract the extension of `filename` and guess its MIME type.
-///
-/// If there is no extension, or the extension has no known MIME association,
-/// `applicaton/octet-stream` is assumed.
-pub fn guess_mime_type_filename(filename: &str) -> Mime {
-    let path = Path::new(filename);
-
-    guess_mime_type(&path)
-}
-
-const MIME_TYPES: &'static str = include_str!("../mime_types.json");
-
-// Lazily initialized task-local hashmap
-// TODO: Make this global since it's read-only after init
-thread_local!(static MIMES: HashMap<String, Mime> = load_mime_types());
 
 /// Get the MIME type associated with a file extension.
 ///
 /// If there is no association for the extension, or `ext` is empty,
 /// `application/octet-stream` is returned.
-pub fn get_mime_type(ext: &str) -> Mime {
-    if ext.is_empty() { return octet_stream(); }
-
-    MIMES.with(|cache| cache.get(ext).cloned()).unwrap_or_else(octet_stream)
+pub fn get_mime_type(search_ext: &str) -> Mime {
+    get_mime_type_str(search_ext)
+        .map(|mime| mime.parse::<Mime>().unwrap())
+        .unwrap_or_else(octet_stream)
 }
 
-/// Load the known mime types from the MIME_TYPES json
-fn load_mime_types() -> HashMap<String, Mime> {
-    let map = if let Json::Object(map) = json::from_str(MIME_TYPES).unwrap() { map }
-    else { unreachable!("MIME types should be supplied as a map!"); };
+/// Get the MIME type string associated with a file extension.
+/// `search_ext` is converted to lowercase for a case-insensitive binary search.
+///
+/// Returns `None` if `search_ext` is empty or an associated extension was not found.
+pub fn get_mime_type_str(search_ext: &str) -> Option<&'static str> {
+    if search_ext.is_empty() { return None; }    
 
-    map.into_iter().filter_map(to_mime_mapping).collect()
-}
+    let search_ext = search_ext.to_ascii_lowercase();
 
-fn to_mime_mapping(val: (String, json::Json)) -> Option<(String, Mime)> {
-    if let (st, Json::String(mime)) = val {
-        if st.char_at(0) == '_' { return None; }
-
-        if let Some(mime) = from_str::<Mime>(&*mime) {
-            return Some((st, mime))
-        }
-    }
-
-    None
+    MIME_TYPES.binary_search_by(|&(ext, _)| ext.cmp(&search_ext))
+        .ok().map(|idx| MIME_TYPES[idx].1)
 }
 
 /// Get the MIME type for `application/octet-stream` (generic binary stream)
 pub fn octet_stream() -> Mime {
-    Mime(TopLevel::Application, SubLevel::Ext("octet-stream".into_string()), Vec::new())
+    "application/octet-stream".parse().unwrap()
 }
 
-#[test]
-fn test_mime_type_guessing() {
-    assert!(get_mime_type("gif").to_string() == "image/gif".to_string());
-    assert!(get_mime_type("txt").to_string() == "text/plain".to_string());
-    assert!(get_mime_type("blahblah").to_string() == "application/octet-stream".to_string());
+#[cfg(test)]
+mod tests {
+    use mime::Mime;
+    use super::{get_mime_type, MIME_TYPES};
+
+    #[test]
+    fn test_mime_type_guessing() {
+        assert_eq!(get_mime_type("gif").to_string(), "image/gif".to_string());
+        assert_eq!(get_mime_type("txt").to_string(), "text/plain".to_string());
+        assert_eq!(get_mime_type("blahblah").to_string(), "application/octet-stream".to_string());
+    }
+
+    #[test]
+    fn test_are_extensions_sorted() {
+        // To make binary search work, extensions need to be sorted in ascending order.
+    	for (curr, next) in MIME_TYPES.iter().zip(MIME_TYPES.iter().skip(1)) {
+    		assert!(curr <= next, "MIME type mappings are not sorted! Failed assert: {:?} <= {:?}", curr, next);
+    	}
+    }
+
+    #[test]
+    fn test_are_mime_types_parseable() {
+        for &(_, mime) in MIME_TYPES {
+            mime.parse::<Mime>().unwrap();
+        }
+    }
 }
