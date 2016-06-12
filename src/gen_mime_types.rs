b372badcf2cc39d2ddc45d191b83e100756189c3
Austin Bonander <austin.bonander@gmail.com>
2016-06-12 06:10:46 -0700

    Reverse mappings with wildcards

--- src/gen_mime_types.rs
+++ src/gen_mime_types.rs
@@ -2,6 +2,7 @@ extern crate phf_codegen;
 extern crate unicase;
 
 use phf_codegen::Map as PhfMap;
+use phf_codegen::OrderedMap as PhfOrderedMap;
 
 use unicase::UniCase;
 
@@ -9,7 +10,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufWriter;
 
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
 use mime_types::MIME_TYPES;
 
@@ -26,6 +27,8 @@ fn main() {
     build_rev_map(&mut outfile);
 }
 
+
+
 // Build forward mappings (ext -> mime type)
 fn build_forward_map<W: Write>(out: &mut W) {
     write!(out, "static MIME_TYPES: phf::Map<UniCase<&'static str>, &'static str> = ").unwrap();
@@ -40,26 +43,61 @@ fn build_forward_map<W: Write>(out: &mut W) {
     writeln!(out, ";").unwrap();
 }
 
+
 // Build reverse mappings (mime type -> ext)
 fn build_rev_map<W: Write>(out: &mut W) {
     // First, collect all the mime type -> ext mappings)
-    let mut dyn_map = HashMap::new();
+    let mut dyn_map = BTreeMap::new();
 
     for &(key, val) in MIME_TYPES {
-        dyn_map.entry(UniCase(val)).or_insert_with(Vec::new).push(key);
+        let (top, sub) = split_mime(val);
+        dyn_map.entry(UniCase(top))
+            .or_insert_with(BTreeMap::new)
+            .entry(UniCase(sub))
+            .or_insert_with(Vec::new)
+            .push(key);
     }
+    
+    write!(out, "static REV_MAPPINGS: phf::Map<UniCase<&'static str>, TopLevelExts> = ").unwrap();
 
-    write!(out, "static REV_MAPPINGS: phf::Map<UniCase<&'static str>, &'static [&'static str]> = ").unwrap();
+    let mut rev_map = PhfMap::new();
 
-    let mut rev_map = PhfMap::new(); 
+    let mut exts = Vec::new();
 
-    for (key, vals) in dyn_map {
-        // FIXME: ugly substitute to force coercion (type ascription doesn't work) 
-        // Workaround unnecessary if this issue is fixed: https://github.com/rust-lang/rust/issues/31260
-        rev_map.entry(key, &format!("{{const VALS: &'static [&'static str] = &{:?}; VALS}}", vals));        
+    for (top, subs) in dyn_map {
+        let top_start = exts.len();
+
+        let mut sub_map = PhfMap::new();
+        
+        for(sub, sub_exts) in subs {
+            let sub_start = exts.len();
+            exts.extend(sub_exts);
+            let sub_end = exts.len();
+
+            sub_map.entry(sub, &format!("({}, {})", sub_start, sub_end));                       
+        }
+
+        let top_end = exts.len();
+
+        let mut subs_bytes = Vec::<u8>::new();
+        sub_map.build(&mut subs_bytes).unwrap();
+
+        let subs_str = ::std::str::from_utf8(&subs_bytes).unwrap();
+        
+        rev_map.entry(
+            top, 
+            &format!("TopLevelExts {{ start: {}, end: {}, subs: {} }}", top_start, top_end, subs_str)
+        );
     }
 
     rev_map.build(out).unwrap();
     writeln!(out, ";").unwrap();
+
+    writeln!(out, "const EXTS: &'static [&'static str] = &{:?};", exts).unwrap();
+}
+
+fn split_mime(mime: &str) -> (&str, &str) {
+    let split_idx = mime.find('/').unwrap();
+    mime.split_at(split_idx)
 }
 
