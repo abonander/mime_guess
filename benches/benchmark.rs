6bfbb7cc8ae0b66971645def5a6c0923fcff5bd9
Austin Bonander <austin.bonander@gmail.com>
2025-09-09 14:40:30 -0700

    feat: improve benchmark

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,29 +1,60 @@
 #[macro_use]
 extern crate criterion;
 extern crate mime_guess;
+extern crate rand_pcg;
+extern crate rand;
 
+use std::env;
+use std::sync::LazyLock;
+use rand::seq::IndexedRandom;
+use rand_pcg::Pcg64Mcg;
 use self::criterion::Criterion;
 
-use mime_guess::from_ext;
+#[path = "../src/mime_types.rs"]
+mod mime_types;
 
-include!("../src/mime_types.rs");
+/// Get a reproducible, seeded RNG.
+fn rng_seeded() -> Pcg64Mcg {
+    static SEED: LazyLock<u128> = LazyLock::new(|| {
+        if let Ok(seed) = env::var("BENCH_RNG_SEED") {
+            return seed.parse().expect("failed to parse BENCH_RNG_SEED");
+        }
+
+        let seed = rand::random();
+
+        eprintln!("Generated random seed; set BENCH_RNG_SEED={seed} to reproduce results");
+
+        seed
+    });
+
+    Pcg64Mcg::new(*SEED)
+}
 
 /// WARNING: this may take a while!
 fn bench_mime_str(c: &mut Criterion) {
+    let mut rng = rng_seeded();
+
     c.bench_function("from_ext", |b| {
-        for (mime_ext, _) in MIME_TYPES {
-            b.iter(|| from_ext(mime_ext).first_raw());
-        }
+
+        b.iter(|| {
+            let (mime_ext, _) = mime_types::MIME_TYPES.choose(&mut rng).unwrap();
+
+            mime_guess::from_ext(mime_ext).first_raw()
+        });
     });
 }
 
 fn bench_mime_str_uppercase(c: &mut Criterion) {
-    c.bench_function("from_ext uppercased", |b| {
-        let uppercased = MIME_TYPES.into_iter().map(|(s, _)| s.to_uppercase());
+    let mut rng = rng_seeded();
 
-        for mime_ext in uppercased {
-            b.iter(|| from_ext(&mime_ext).first_raw());
-        }
+    c.bench_function("from_ext uppercased", |b| {
+        b.iter(|| {
+            let (mime_ext, _) = mime_types::MIME_TYPES.choose(&mut rng).unwrap();
+
+            let mime_ext = mime_ext.to_uppercase();
+
+            mime_guess::from_ext(&mime_ext).first_raw()
+        });
     });
 }
 
